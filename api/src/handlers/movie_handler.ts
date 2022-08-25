83122512067d837724148e9d6e386e8ba3075111
Abdullah <abdullah.fattah808@gmail.com>
2022-08-25 20:50:52 +0200

    CRD Functionality added

--- api/src/handlers/movie_handler.ts
+++ api/src/handlers/movie_handler.ts
@@ -1,40 +1,56 @@
-import MovieModel from "../models/Movie_model";
-import { Request, Response, NextFunction } from "express";
+import MovieModel from '../models/Movie_model'
+import { Request, Response, NextFunction } from 'express'
 
+const store = new MovieModel()
 
-const store = new MovieModel();
-
-
-export const index = async(req:Request, res:Response, next:NextFunction)=>{
-    try {
-        const index = await store.indexMovie();
-        res.json(index)
-
-    } catch (err) {
-            next(err)
-    }
+export const index = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const index = await store.indexMovie()
+    res.json(index)
+  } catch (err) {
+    next(err)
+  }
 }
 
-
-export const create = async(req:Request, res:Response, next:NextFunction)=>{
-   try{
-       const movie = await store.addMovie(req.body);
-       res.json({
-           Action:"Add movie",
-           Data:{...movie},
-           Status:"Success"
-       })
-   }catch(err){
+export const create = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const movie = await store.addMovie(req.body)
+    res.json({
+      Action: 'Add movie',
+      Data: { ...movie },
+      Status: 'Success',
+    })
+  } catch (err) {
     next(err)
-   }
-};
+  }
+}
 
+export const get = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const get = await store.getMovie(req.params.id)
+    res.json(get)
+  } catch (err) {
+    throw new Error(`Unable to get movie: ${err}`)
+  }
+}
 
-export const get = async(req:Request, res: Response, next:NextFunction)=>{
-    try {
-        const get = await store.getMovie(req.params.id);
-        res.json(get)
-    } catch (err) {
-        throw new Error(`Unable to get movie: ${err}`)
-    }
+export const remove = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const remove = await store.deleteMovie(req.params.id)
+    res.json(remove)
+  } catch (err) {
+    throw new Error(`Unable to delete movie: ${err}`)
+  }
 }
--- api/src/routes/api/movie_routes.ts
+++ api/src/routes/api/movie_routes.ts
@@ -1,11 +1,11 @@
-import * as handlers from '../../handlers/movie_handler';
-import express from 'express';
+import * as handlers from '../../handlers/movie_handler'
+import express from 'express'
 
-const routes = express.Router();
+const routes = express.Router()
 
-routes.post("/",handlers.create);
-routes.get("/",handlers.index);
-routes.get("/:id", handlers.get);
+routes.post('/', handlers.create)
+routes.get('/', handlers.index)
+routes.get('/:id', handlers.get)
+routes.delete('/:id', handlers.remove)
 
-
-export default routes;
+export default routes
