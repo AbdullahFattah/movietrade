2170b4e3726f6c0518c8864fc7cf1795cfe9bb3e
Abdullah <abdullah.fattah808@gmail.com>
2022-09-25 16:23:06 +0200

    Initial user profile structure

--- src/components/me.js
+++ src/components/me.js
@@ -6,9 +6,13 @@ export default function Me() {
       <div className="container text-center mt-3">
         <p className="text-warning">Hello, User!</p>
 
-        <div className="my-profile">
-          <p>You</p>
+        <div className="my-profile" align="center">
+          <div className="my-profile-photo">Your picture here</div>
+          <div className="my-info">
+            <p>FirstName LastName</p>
+          </div>
         </div>
+        <hr></hr>
         <div className="my-movies">
           <label>Your movie library</label>
           <div className="row">
